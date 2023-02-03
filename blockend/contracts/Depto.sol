74819b189f91c8ed60e58ff0d97963eef90a4437
Gabriel Antony Xaviour <gabrielantony56@gmail.com>
2023-02-03 07:01:57 +0530

    Depto contract done, few changes for GovernorContract yet to be done

--- blockend/contracts/Depto.sol
+++ blockend/contracts/Depto.sol
@@ -2,21 +2,208 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import {MarketAPI} from "@zondax/filecoin-solidity/contracts/v0.8/MarketAPI.sol";
+import {CommonTypes} from "@zondax/filecoin-solidity/contracts/v0.8/types/CommonTypes.sol";
+import {MarketTypes} from "@zondax/filecoin-solidity/contracts/v0.8/types/MarketTypes.sol";
+import {Actor} from "@zondax/filecoin-solidity/contracts/v0.8/utils/Actor.sol";
+import {Misc} from "@zondax/filecoin-solidity/contracts/v0.8/utils/Misc.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "./governance/GovernorContract.sol";
 
-contract Depto is Ownable {
-    uint256 private value;
+/* 
+Contract Usage
+    Step   |   Who   |    What is happening  |   Why 
+    ------------------------------------------------
+    Deploy | contract owner   | contract owner deploys address is owner who can call addCID  | create contract setting up rules to follow
+    AddCID | data pinners     | set up cids that the contract will incentivize in deals      | add request for a deal in the filecoin network, "store data" function
+    Fund   | contract funders |  add FIL to the contract to later by paid out by deal        | ensure the deal actually gets stored by providing funds for bounty hunter and (indirect) storage provider
+    Claim  | bounty hunter    | claim the incentive to complete the cycle                    | pay back the bounty hunter for doing work for the contract
+*/
+contract Depto is ERC721URIStorage, Ownable {
+    struct PatentStorageDeal {
+        bytes cidraw;
+        uint size;
+        address proposer;
+        address patentClaimer;
+        bool set;
+        uint mintedAt;
+    }
+    mapping(bytes => PatentStorageDeal) public patents;
+    mapping(bytes => mapping(uint64 => bool)) public cidProviders;
+    mapping(address => uint256) public rewards;
+    uint256 public currentApprovedPatentCount;
+    mapping(address => uint256) validationClaim;
 
-    // Emitted when the stored value changes
-    event ValueChanged(uint256 newValue);
+    uint public currentTokenId;
 
-    // Stores a new value in the contract
-    function store(uint256 newValue) public onlyOwner {
-        value = newValue;
-        emit ValueChanged(newValue);
+    address constant CALL_ACTOR_ID = 0xfe00000000000000000000000000000000000005;
+    uint64 constant DEFAULT_FLAG = 0x00000000;
+    uint64 constant METHOD_SEND = 0;
+
+    GovernorContract governorContract;
+
+    uint256 public constant VERIFIER_FEE = 215053763440000000;
+    uint256 public constant VALIDATOR_FEE = 4301075260000000;
+    uint256 public constant SP_FEE = 645161290320000000;
+    uint256 public constant GOVERNMENT_FEE = 667741935480000000;
+    uint256 public constant CREATOR_FEE = 300000000000000000;
+
+    address public constant GOVERNMENT_ADDRESS =
+        0x5353448037eb0d940f209d47f31DbdDd66237A90;
+    address public constant DEPTO_CREATOR_ADDRESS =
+        0x5353448037eb0d940f209d47f31DbdDd66237A90;
+
+    constructor(address payable governorContractAddress)
+        ERC721("Depto", "DPT")
+    {
+        governorContract = GovernorContract(governorContractAddress);
+        currentApprovedPatentCount = 0;
     }
 
-    // Reads the last stored value
-    function retrieve() public view returns (uint256) {
-        return value;
+    receive() external payable {
+        rewards[DEPTO_CREATOR_ADDRESS] += msg.value;
+    }
+
+    fallback() external payable {
+        rewards[DEPTO_CREATOR_ADDRESS] += msg.value;
+    }
+
+    function claimFeeRewards() public {
+        require(
+            governorContract.isDAOMember(msg.sender) ||
+                msg.sender == GOVERNMENT_ADDRESS ||
+                msg.sender == DEPTO_CREATOR_ADDRESS,
+            "Not authorized to claim rewards"
+        );
+        uint reward;
+        bool success;
+        bytes memory data;
+        if (governorContract.isDAOMember(msg.sender)) {
+            if (currentApprovedPatentCount - validationClaim[msg.sender] > 0) {
+                reward =
+                    VALIDATOR_FEE *
+                    (currentApprovedPatentCount - validationClaim[msg.sender]);
+                validationClaim[msg.sender] = currentApprovedPatentCount;
+                (success, data) = payable(msg.sender).call{value: reward}("");
+            }
+        }
+        reward = rewards[msg.sender];
+        rewards[msg.sender] = 0;
+        (success, data) = payable(msg.sender).call{value: reward}("");
+    }
+
+    function addCID(
+        bytes calldata cidraw,
+        uint size,
+        address proposer,
+        address patentClaimer
+    ) public payable returns (bool) {
+        require(msg.value >= 2000000000000000000, "Invalid");
+        patents[cidraw] = PatentStorageDeal(
+            cidraw,
+            size,
+            proposer,
+            patentClaimer,
+            false,
+            0
+        );
+
+        return true;
+    }
+
+    function policyOK(bytes memory cidraw, uint64 provider)
+        internal
+        view
+        returns (bool)
+    {
+        bool alreadyStoring = cidProviders[cidraw][provider];
+        return !alreadyStoring;
+    }
+
+    function authorizeData(
+        bytes memory cidraw,
+        uint64 provider,
+        uint size
+    ) internal {
+        require(patents[cidraw].set, "cid must be added before authorizing");
+        require(patents[cidraw].size == size, "data size must match expected");
+        require(
+            policyOK(cidraw, provider),
+            "deal failed policy check: has provider already claimed this cid?"
+        );
+
+        cidProviders[cidraw][provider] = true;
+    }
+
+    function claim_bounty(uint64 deal_id) public {
+        MarketTypes.GetDealDataCommitmentReturn memory commitmentRet = MarketAPI
+            .getDealDataCommitment(deal_id);
+        MarketTypes.GetDealProviderReturn memory providerRet = MarketAPI
+            .getDealProvider(deal_id);
+
+        authorizeData(
+            commitmentRet.data,
+            providerRet.provider,
+            commitmentRet.size
+        );
+
+        // get dealer (bounty hunter client)
+        MarketTypes.GetDealClientReturn memory clientRet = MarketAPI
+            .getDealClient(deal_id);
+        patents[cidraw].set = true;
+        patents[cidraw].mintedAt = block.timestamp;
+        // send reward to client
+        send(clientRet.client);
+
+        // change state from waiting for SP to Success
+
+        // allot money to everyone
+        rewards[GOVERNMENT_ADDRESS] += GOVERNMENT_FEE;
+        rewards[patents[commitmentRet.data].proposer] += VERIFIER_FEE;
+        rewards[DEPTO_CREATOR_ADDRESS] += CREATOR_FEE;
+        currentApprovedPatentCount += 1;
+
+        //mint patent NFT to patent applicant
+        _mint(patents[commitmentRet.data].patentClaimer, currentTokenId);
+        _setTokenURI(currentTokenId, string(commitmentRet.data));
+    }
+
+    function call_actor_id(
+        uint64 method,
+        uint256 value,
+        uint64 flags,
+        uint64 codec,
+        bytes memory params,
+        uint64 id
+    )
+        public
+        returns (
+            bool,
+            int256,
+            uint64,
+            bytes memory
+        )
+    {
+        (bool success, bytes memory data) = address(CALL_ACTOR_ID).delegatecall(
+            abi.encode(method, value, flags, codec, params, id)
+        );
+        (int256 exit, uint64 return_codec, bytes memory return_value) = abi
+            .decode(data, (int256, uint64, bytes));
+        return (success, exit, return_codec, return_value);
+    }
+
+    // send 1 FIL to the filecoin actor at actor_id
+    function send(uint64 actorID) internal {
+        bytes memory emptyParams = "";
+        delete emptyParams;
+
+        call_actor_id(
+            METHOD_SEND,
+            SP_FEE,
+            DEFAULT_FLAG,
+            Misc.NONE_CODEC,
+            emptyParams,
+            actorID
+        );
     }
 }
--- blockend/contracts/governance/GovernorContract.sol
+++ blockend/contracts/governance/GovernorContract.sol
@@ -3,136 +3,234 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/governance/Governor.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol";
-import "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
-import "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorTimelockControl.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorSettings.sol";
 
 contract GovernorContract is
-  Governor,
-  GovernorSettings,
-  GovernorCountingSimple,
-  GovernorVotes,
-  GovernorVotesQuorumFraction,
-  GovernorTimelockControl
+    Governor,
+    GovernorSettings,
+    GovernorCountingSimple,
+    GovernorTimelockControl
 {
-  constructor(
-    IVotes _token,
-    TimelockController _timelock,
-    uint256 _quorumPercentage,
-    uint256 _votingPeriod,
-    uint256 _votingDelay
-  )
-    Governor("GovernorContract")
-    GovernorSettings(
-      _votingDelay, /* 1 block */ // voting delay
-      _votingPeriod, // 45818, /* 1 week */ // voting period
-      0 // proposal threshold
+    struct DAOMember {
+        string name;
+        address walletAddress;
+        uint joinedAt;
+    }
+
+    struct Application {
+        string name;
+        address walletAddress;
+        string metadataURI;
+        uint appliedOn;
+    }
+
+    struct FalseClaim {
+        string name;
+        address claimerWalletAddress;
+        uint applierTokenId;
+        uint falsePatentTokenId;
+        string metadata;
+        uint claimedOn;
+    }
+
+    DAOMember[] public daoMembers;
+    uint public daoMemberPointer;
+    mapping(uint => Application) applications;
+    uint public applicationsPointer;
+    mapping(uint => FalseClaim) falseClaims;
+    uint public falseClaimPointer;
+    mapping(address => bool) public isDAOMember;
+
+    mapping(address => uint256[]) public userToPatentClaims;
+
+    uint256 public constant PROPOSAL_FEE = 2000000000000000000;
+    uint256 public constant FALSE_CLAIM_FEE = 1000000000000000000;
+
+    constructor(
+        TimelockController _timelock,
+        uint256 _votingPeriod,
+        uint256 _votingDelay
     )
-    GovernorVotes(_token)
-    GovernorVotesQuorumFraction(_quorumPercentage)
-    GovernorTimelockControl(_timelock)
-  {}
+        Governor("GovernorContract")
+        GovernorSettings(
+            _votingDelay, /* 1 block */ // voting delay
+            _votingPeriod, // 45818, /* 1 week */ // voting period
+            0 // proposal threshold
+        )
+        GovernorTimelockControl(_timelock)
+    {
+        daoMemberPointer = 0;
+    }
 
-  function votingDelay()
-    public
-    view
-    override(IGovernor, GovernorSettings)
-    returns (uint256)
-  {
-    return super.votingDelay();
-  }
+    modifier checkDAOMember(address caller) {
+        require(
+            isDAOMember[caller],
+            "GovernorContract: Only DAO members can access this function"
+        );
+        _;
+    }
 
-  function votingPeriod()
-    public
-    view
-    override(IGovernor, GovernorSettings)
-    returns (uint256)
-  {
-    return super.votingPeriod();
-  }
+    function votingDelay()
+        public
+        view
+        override(IGovernor, GovernorSettings)
+        returns (uint256)
+    {
+        return super.votingDelay();
+    }
 
-  // The following functions are overrides required by Solidity.
+    function votingPeriod()
+        public
+        view
+        override(IGovernor, GovernorSettings)
+        returns (uint256)
+    {
+        return super.votingPeriod();
+    }
 
-  function quorum(uint256 blockNumber)
-    public
-    view
-    override(IGovernor, GovernorVotesQuorumFraction)
-    returns (uint256)
-  {
-    return super.quorum(blockNumber);
-  }
+    // The following functions are overrides required by Solidity.
 
-  function getVotes(address account, uint256 blockNumber)
-    public
-    view
-    override(IGovernor, Governor)
-    returns (uint256)
-  {
-    return super.getVotes(account, blockNumber);
-  }
+    function state(uint256 proposalId)
+        public
+        view
+        override(Governor, GovernorTimelockControl)
+        returns (ProposalState)
+    {
+        return super.state(proposalId);
+    }
 
-  function state(uint256 proposalId)
-    public
-    view
-    override(Governor, GovernorTimelockControl)
-    returns (ProposalState)
-  {
-    return super.state(proposalId);
-  }
+    function applyPatent(string name, string metadataURI) public payable {
+        require(
+            msg.value >= PROPOSAL_FEE,
+            "Insufficient Fee to apply for a patent"
+        );
+        // TODO: Remaining money handling
+        applications[applicationsPointer] = Application(
+            name,
+            msg.sender,
+            metadataURI,
+            block.timestamp
+        );
+        applicationsPointer += 1;
+    }
 
-  function propose(
-    address[] memory targets,
-    uint256[] memory values,
-    bytes[] memory calldatas,
-    string memory description
-  ) public override(Governor, IGovernor) returns (uint256) {
-    return super.propose(targets, values, calldatas, description);
-  }
+    function applyFalseClaim(
+        string name,
+        uint applierPatentTokenId,
+        uint falsePatentTokenId,
+        string metadataURI
+    ) public payable {
+        require(
+            msg.value >= FALSE_CLAIM_FEE,
+            "Insufficient Funds for False Claim"
+        );
+        falseClaims[falseClaimPointer] = FalseClaim(
+            name,
+            msg.sender,
+            applierPatentTokenId,
+            falsePatentTokenId,
+            metadataURI,
+            block.timestamp
+        );
+        falseClaimPointer += 1;
+    }
 
-  function proposalThreshold()
-    public
-    view
-    override(Governor, GovernorSettings)
-    returns (uint256)
-  {
-    return super.proposalThreshold();
-  }
+    function proposePatent(
+        address[] memory targets,
+        uint256[] memory values,
+        bytes[] memory calldatas,
+        string memory description
+    ) public checkDAOMember(msg.sender) {
+        require(values[0] >= PROPOSAL_FEE, "Insufficient FEE to propose");
+        require(checkDAOMember(targets[1]), "Proposal format invalid");
+        super.propose(addresses, values, calldatas, description);
+    }
 
-  function _execute(
-    uint256 proposalId,
-    address[] memory targets,
-    uint256[] memory values,
-    bytes[] memory calldatas,
-    bytes32 descriptionHash
-  ) internal override(Governor, GovernorTimelockControl) {
-    super._execute(proposalId, targets, values, calldatas, descriptionHash);
-  }
+    function proposeMember(
+        address[] memory targets,
+        uint256[] memory values,
+        bytes[] memory calldatas,
+        string memory description
+    ) public checkDAOMember(msg.sender) {
+        require(
+            daoMemberPointer < 40,
+            "Cannot add any more members in the DAO"
+        );
+        super.propose(addresses, values, calldatas, description);
+    }
 
-  function _cancel(
-    address[] memory targets,
-    uint256[] memory values,
-    bytes[] memory calldatas,
-    bytes32 descriptionHash
-  ) internal override(Governor, GovernorTimelockControl) returns (uint256) {
-    return super._cancel(targets, values, calldatas, descriptionHash);
-  }
+    function proposeFalseClaim(
+        address[] memory targets,
+        uint256[] memory values,
+        bytes[] memory calldatas,
+        string memory description
+    ) public checkDAOMember(msg.sender) {
+        require(
+            daoMemberPointer < 40,
+            "Cannot add any more members in the DAO"
+        );
+        super.propose(addresses, values, calldatas, description);
+    }
 
-  function _executor()
-    internal
-    view
-    override(Governor, GovernorTimelockControl)
-    returns (address)
-  {
-    return super._executor();
-  }
+    function propose(
+        address[] memory targets,
+        uint256[] memory values,
+        bytes[] memory calldatas,
+        string memory description
+    ) public override(Governor, IGovernor) returns (uint256) {}
 
-  function supportsInterface(bytes4 interfaceId)
-    public
-    view
-    override(Governor, GovernorTimelockControl)
-    returns (bool)
-  {
-    return super.supportsInterface(interfaceId);
-  }
+    function proposalThreshold()
+        public
+        view
+        override(Governor, GovernorSettings)
+        returns (uint256)
+    {
+        return super.proposalThreshold();
+    }
+
+    function _execute(
+        uint256 proposalId,
+        address[] memory targets,
+        uint256[] memory values,
+        bytes[] memory calldatas,
+        bytes32 descriptionHash
+    ) internal override(Governor, GovernorTimelockControl) {
+        super._execute(proposalId, targets, values, calldatas, descriptionHash);
+    }
+
+    function _cancel(
+        address[] memory targets,
+        uint256[] memory values,
+        bytes[] memory calldatas,
+        bytes32 descriptionHash
+    ) internal override(Governor, GovernorTimelockControl) returns (uint256) {
+        return super._cancel(targets, values, calldatas, descriptionHash);
+    }
+
+    function _executor()
+        internal
+        view
+        override(Governor, GovernorTimelockControl)
+        returns (address)
+    {
+        return super._executor();
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(Governor, GovernorTimelockControl)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function getPatentClaims(address user)
+        public
+        view
+        returns (uint256[] memory)
+    {
+        return userToPatentClaims[user];
+    }
 }
